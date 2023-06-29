e4675c5c3439859ca445bd012835f48c07e58ed0
Chee Hou Ng <ngcheehou1988@outlook.com>
2023-06-29 23:27:25 +0800

    1

---
+++ src/WebApp2/DataLibrary/Identity/MyDepartment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Identity
+{
+    public class MyDepartment : IdentityRole
+    {
+
+        public WorkMode JobType { get; set; }
+        //public virtual ICollection<MyEmployeeDepartment>? MyEmployeeDepartment { get; set; }
+
+        //public virtual ICollection<MyDepartmentClaim>? MyDepartmentClaim { get; set; }
+
+        //public MyDepartment() : base()
+        //{
+
+        //}
+
+    }
+}
